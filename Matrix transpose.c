aef83d9731981b0c876ea7badf93fc2dac962828
junaid0520-aaub <mdjunaid0520@gmail.com>
2025-11-25 00:14:45 +0600

    Add files via upload

---
+++ Matrix transpose.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+  int a[100][100],b[100][100],i,j,r,c;
+
+  printf("Enter the number of row : ");
+  scanf("%d",&r);
+
+  printf("Enter the number of collum : ");
+  scanf("%d",&c);
+
+  for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+      printf("a[%d][%d] = ",i+1,j+1);
+      scanf("%d",&a[i][j]);
+    }
+  }
+
+  for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+      printf(" %d",a[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("Transpose matrix is \n");
+
+  for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+      printf(" %d",b[i][j]=a[j][i]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+
+}
