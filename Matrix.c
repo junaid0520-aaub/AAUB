def2b9756914f80bc017713cc01a9102ad30ac54
junaid0520-aaub <mdjunaid0520@gmail.com>
2025-11-25 00:28:17 +0600

    Matrix.c

--- Matrix.c
+++ Matrix.c
@@ -6,7 +6,7 @@ int main()
   printf("Enter the number of row : ");
   scanf("%d",&r);
 
-  printf("Enter the number of collum : ");
+  printf("Enter the number of column : ");
   scanf("%d",&c);
 
   for(i=0;i<r;i++)
@@ -30,3 +30,4 @@ int main()
     return 0;
 
 }
+
